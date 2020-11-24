804ee7c64734dccb0040f8c66f133ec0c3bd863e
Phamtung1802 <phamtung.0218@gmail.com>
2020-11-24 08:16:41 +0700

    first commit

---
+++ src/main/java/tung/codegym/tung/TungApplication.java
@@ -0,0 +1,13 @@
+package tung.codegym.tung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tung/codegym/tung/TungApplicationTests.java
@@ -0,0 +1,13 @@
+package tung.codegym.tung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
